a95b6e46e9bc4150f2473b1dcb28303331bb85ec
vansteki <vansteki.tw@gmail.com>
2015-06-13 13:32:26 +0800

    add static template

--- app/main.js
+++ app/main.js
@@ -1,37 +1,85 @@
-/* main.js */
-
-'use strict';
-
-// require("expose?jQuery!jquery");
 var React = require('react');
-var TestOne = require('./TestOne.js');
-var TestTwo = require('./TestTwo.js');
 
+var ProductCategoryRow = React.createClass({
+    render: function() {
+        return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+    }
+});
 
-var Main = React.createClass({
-    getInitialState: function() {
-        return {
-          switch: true
-        };
-    },
-    _toggle() {
-        this.setState({
-            switch: !this.state.switch
+var ProductRow = React.createClass({
+    render: function() {
+        var name = this.props.product.stocked ?
+            this.props.product.name :
+            <span style={{color: 'red'}}>
+                {this.props.product.name}
+            </span>;
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        );
+    }
+});
+
+var ProductTable = React.createClass({
+    render: function() {
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product) {
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
         });
-    },
-    _getUser(e, n, url) {
-        var url = url || 'http://jsonplaceholder.typicode.com/users/1';
-        jQuery.getJSON(url, function(res) { console.log(res)});
-    },
-    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+});
+
+var SearchBar =  React.createClass({
+    render: function() {
+        return (
+            <form>
+                <input type='text' placeholder='Searching...'/>
+                <p>
+                    <input type='checkbox' />
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
+});
+
+var FilterableProductTable = React.createClass({
+    render: function() {
         return (
             <div>
-                <input type="button" onClick={this._toggle} value="Press Me!"/>
-                <input type="button" onClick={this._getUser} value="ckUser"/>
-                {this.state.switch ? <TestOne /> : <TestTwo />}
+                <SearchBar />
+                <ProductTable products={this.props.products} />
             </div>
         );
     }
 });
 
-React.render(<Main />, document.body);
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+
+React.render(<FilterableProductTable products={PRODUCTS}/>, document.body);
